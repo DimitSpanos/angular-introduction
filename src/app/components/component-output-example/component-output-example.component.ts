d744b06ea78ccddc77d13b82dff2531e8349172a
D. Spanos <dimitrispanos21@hotmail.com>
2024-04-08 18:12:46 +0300

    Angular Material

--- src/app/components/component-output-example/component-output-example.component.ts
+++ src/app/components/component-output-example/component-output-example.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { EPerson, ManyPerson } from 'src/app/shared/interfaces/person';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+import {Dialog, DialogRef, DIALOG_DATA, DialogModule} from '@angular/cdk/dialog';
+import {FormsModule} from '@angular/forms';
+import { PersonTableComponent } from '../person-table/person-table.component';
+
 
 @Component({
   selector: 'app-component-output-example',
@@ -12,8 +16,14 @@ import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.c
 export class ComponentOutputExampleComponent {
   manyPerson = ManyPerson
 
+  constructor(public dialog: Dialog) {}
+
+
   onPersonClicked(person: EPerson) {
-    alert(this.personTemplate(person))
+    // alert(this.personTemplate(person))
+    this.dialog.open(PersonDialogComponent, {
+      data: person,
+    })
   }
 
   personTemplate(person: EPerson) {
@@ -24,6 +34,31 @@ export class ComponentOutputExampleComponent {
       Email: ${person.email}
       Education: ${person.education}`
   }
+}
+
+@Component({
+  imports: [DialogModule, PersonTableComponent],
+  standalone: true,
+  template: `<app-person-table [person]="person"></app-person-table>
+  <button class="btn btn-primary btn-sm " (click)="dialogRef.close()">Close</button>`,
+  styles: [
+    `
+    :host {
+    display: block;
+    background: #fff;
+    border-radius: 8px;
+    padding: 16px;
+    max-width: 500px;
+    }
+    `,
+    ],
+  
+})
+class PersonDialogComponent {
+  constructor(
+    public dialogRef: DialogRef<PersonDialogComponent>,
+    @Inject(DIALOG_DATA) public person: EPerson
+  ) {}
 }
 
 
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Person } from 'src/app/shared/interfaces/person';
+import { EPerson, Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
@@ -9,5 +9,13 @@ import { Person } from 'src/app/shared/interfaces/person';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  @Input() person: Person | undefined //dhlwnoume apla oti einai input xwris na exoume perasei - orisei data  
+  @Input() person: Person | EPerson | undefined //dhlwnoume apla oti einai input xwris na exoume perasei - orisei data  
+
+  isPerson() {
+    return this.person && 'address' in this.person //elegxoume an to object exei to key address
+  }
+
+  isEPerson() {
+    return this.person && 'education' in this.person //elegxoume an to object exei to key education
+  }
 }
